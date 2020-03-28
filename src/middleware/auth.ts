7866d0307f77b61b29448a2eff471a1190b22d85
AhmedHossam01 <ahmed.1318039@stemdakahlia.moe.edu.eg>
2020-03-28 12:48:34 +0200

    role based authorization v2

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,28 +3,34 @@ require("dotenv").config();
 import { Request, Response, NextFunction } from "express";
 import throwErr from "../util/errHandler";
 
-export const auth = (roles?: string[]) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const token = req.headers.token;
+export const auth = (roles?: string[]) => (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.headers.token;
 
-      // @ts-ignore
-      const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    // @ts-ignore
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
 
-      if (roles && roles.includes(decoded.role)) {
-        console.log(roles);
-        next();
-      }
-
-      if (roles && !roles.includes(decoded.role)) {
-        return throwErr(401, "Unauthorized", next);
-      }
-
-      if (!roles) {
-        next();
-      }
-    } catch (err) {
-      throwErr(401, "Auth Failed", next);
+    if (roles && roles.includes(decoded.role)) {
+      next();
     }
-  };
+
+    if (!roles) {
+      next();
+    }
+
+    if (roles && !roles.includes(decoded.role)) {
+      return throwErr(401, "Unauthorized", next);
+    }
+
+    /** I was able to do it like this
+     * If (user.role >= givenRole) pass
+     * انا فاجر اوي يعني
+     */
+  } catch (err) {
+    throwErr(401, "Auth Failed", next);
+  }
 };
